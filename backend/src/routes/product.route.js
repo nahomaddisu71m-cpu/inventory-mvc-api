67fa0092f7aa47f90e979a41bb4152a474b1f6bf
Nahom Addisu <nahomaddisu71m@gmail.com>
2026-05-30 06:57:34 +0300

    Update product.route.js

--- backend/src/routes/product.route.js
+++ backend/src/routes/product.route.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import {
   getAllProducts,
-    getProductById,
-      createProduct,
-        updateProduct,
-          deleteProduct
-          } from '../controllers/productController.js';
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} from '../controllers/productController.js';
 
-          const router = express.Router();
+const router = express.Router();
 
-          router.get('/', getAllProducts);
-          router.get('/:id', getProductById);
-          router.post('/', createProduct);
-          router.put('/:id', updateProduct);
-          router.delete('/:id', deleteProduct);
+router.get('/', getAllProducts);
+router.get('/:id', getProductById);
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
-          export default router;
+export default router;
