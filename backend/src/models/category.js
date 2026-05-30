1e17a99c9aea7db8629a1cd6bf0834b6f0aa98c3
Nahom Addisu <nahomaddisu71m@gmail.com>
2026-05-30 06:54:47 +0300

    Update category.js

--- backend/src/models/category.js
+++ backend/src/models/category.js
@@ -1,7 +1,6 @@
 let categories = [
-      { id: 1, name: "Electronics", description: "Electronic devices and gadgets" },
-        { id: 2, name: "Accessories", description: "Computer and mobile accessories" }
-        ];
+  { id: 1, name: "Electronics", description: "Electronic devices and gadgets" },
+  { id: 2, name: "Accessories", description: "Computer and mobile accessories" }
+];
 
-        export default { categories };
-]
+export default { categories };
